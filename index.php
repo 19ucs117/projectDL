5e3a3ab69435555109a44a59897e296a5cad1c8d
19ucs117 <77544652+19ucs117@users.noreply.github.com>
2021-02-23 02:34:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ if (isset($_POST['uname']) && isset($_POST['pswd'])) {
     <a href="#">
       <img src="loyola_icon.png" alt="hpage">
     </a>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
   </head>
   <body>
     <div class="login">
